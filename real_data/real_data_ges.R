9d1e3c71efa35b3e9cafaa124d3c035a1a7ba68d
Guanxun Li <victorguanxun@gmail.com>
2021-11-12 16:41:08 -0600

    Check real data results.

---
+++ real_data/real_data_ges.R
@@ -0,0 +1,36 @@
+## load data
+dta <- readRDS("dta_use.rds")
+dta_group <- read.csv("raw_data/dta.csv")
+dta_group <- dta_group[, -1]
+rownames(dta_group) <- dta_group$AOCSID
+sample_group <- dta_group[rownames(dta), 8]
+sample_group <- ifelse(sample_group == "1", 1, 0)
+dta_1 <- dta[which(sample_group == 1), ]
+dta_2 <- dta[which(sample_group == 0), ]
+
+## GES method
+library(pcalg)
+# data set 1
+score_ges <- new("GaussL0penObsScore", data = dta_1, intercept = FALSE)
+ges_fit1 <- ges(score_ges)
+ges_adj1 <- as(ges_fit1$repr, "matrix")
+ges_adj1 <- ifelse(ges_adj1 == TRUE, 1, 0)
+ges_adj1_u <- ceiling((ges_adj1 + t(ges_adj1)) / 2)
+sum(ges_adj1_u) / 2
+# data set 2
+score_ges <- new("GaussL0penObsScore", data = dta_2, intercept = FALSE)
+ges_fit2 <- ges(score_ges)
+ges_adj2 <- as(ges_fit2$repr, "matrix")
+ges_adj2 <- ifelse(ges_adj2 == TRUE, 1, 0)
+ges_adj2_u <- ceiling((ges_adj2 + t(ges_adj2)) / 2)
+sum(ges_adj2_u) / 2
+# Intersection
+length(intersect(which(ges_adj1_u == 1), which(ges_adj2_u == 1))) / 2
+
+## get center
+ges_adj <- ceiling((ges_adj1 + ges_adj2) / 2)
+c_row <- rowSums(ges_adj)
+c_col <- colSums(ges_adj)
+c_sum <- c_row + c_col
+names(c_sum) <- colnames(dta_1)
+head(sort(c_sum, decreasing = TRUE))
---
+++ real_data/real_data_mcmc.R
@@ -0,0 +1,69 @@
+## load data
+dta <- readRDS("dta_use.rds")
+dta_group <- read.csv("raw_data/dta.csv")
+dta_group <- dta_group[, -1]
+rownames(dta_group) <- dta_group$AOCSID
+sample_group <- dta_group[rownames(dta), 8]
+sample_group <- ifelse(sample_group == "1", 1, 0)
+dta_1 <- dta[which(sample_group == 1), ]
+dta_2 <- dta[which(sample_group == 0), ]
+
+## generate graph
+source("method_code/Graph_MCMC_two.R")
+## get order
+dta <- rbind(dta_1, dta_2)
+library(pcalg)
+score_ges <- new("GaussL0penObsScore", data = dta, intercept = TRUE)
+ges_fit <- ges(score_ges)
+ges_adj <- as(ges_fit$repr, "matrix")
+ges_adj <- ifelse(ges_adj == TRUE, 1, 0)
+graph_i <- igraph::graph_from_adjacency_matrix(ges_adj, mode = "directed", diag = FALSE)
+order_int <- as.numeric(igraph::topo_sort(graph_i))
+## Do MCMC
+out_res <- Graph_MCMC_two(dta_1, dta_2,
+                          scale_x = FALSE, intercept = TRUE,
+                          order_int = order_int, iter_max = 50000, sigma02_int = NULL, sigma2_int = NULL,
+                          prior_vec = NULL, itermax = 100, tol = 1e-4, sigma0_low_bd = 1e-8, burn_in = 40000
+)
+saveRDS(out_res, "out_res.rds")
+
+## check results
+out_res <- readRDS("out_res.rds")
+p <- ncol(dta)
+alpha_mat_1 <- matrix(0, nrow = p, ncol = p)
+alpha_mat_2 <- matrix(0, nrow = p, ncol = p)
+A_mat_1 <- matrix(0, nrow = p, ncol = p)
+A_mat_2 <- matrix(0, nrow = p, ncol = p)
+iter_max <- length(out_res[[1]])
+for (iter in seq_len(iter_max)) {
+  order_tmp <- order(out_res$order_list[[iter]])
+  alpha_mat_1 <- alpha_mat_1 + out_res$alpha_list_1[[iter]][order_tmp, order_tmp]
+  alpha_mat_2 <- alpha_mat_2 + out_res$alpha_list_2[[iter]][order_tmp, order_tmp]
+  A_mat_1 <- A_mat_1 + out_res$A_list_1[[iter]][order_tmp, order_tmp]
+  A_mat_2 <- A_mat_2 + out_res$A_list_2[[iter]][order_tmp, order_tmp]
+}
+alpha_mat_1 <- alpha_mat_1 / iter_max
+alpha_mat_2 <- alpha_mat_2 / iter_max
+A_mat_1 <- A_mat_1 / iter_max
+A_mat_2 <- A_mat_2 / iter_max
+
+#### Compare results
+## data set 1
+adj_1 <- ifelse(alpha_mat_1 > 0.5, 1, 0)
+adj_1 <- t(adj_1)
+sum(adj_1)
+## data set 2
+adj_2 <- ifelse(alpha_mat_2 > 0.5, 1, 0)
+adj_2 <- t(adj_2)
+sum(adj_2)
+## check results
+length(intersect(which(adj_1 == 1), which(adj_2 == 1)))
+
+#### check centers
+## get center
+mcmc_adj <- ceiling((adj_1 + adj_2) / 2)
+c_row <- rowSums(mcmc_adj)
+c_col <- colSums(mcmc_adj)
+c_sum <- c_row + c_col
+names(c_sum) <- colnames(dta_1)
+head(sort(c_sum, decreasing = TRUE))
